8513b77ca2bbcf804fc6d4e2c9ea80237d3e7093
Peter Müller <peter@crycode.de>
2022-07-26 13:19:44 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,78 @@
+# ESLint config
+
+This modules provides an ESLint configuration to be used with TypeScript
+according to my personal preferences.
+
+## Usage
+
+Add `eslint` and `@crycode/eslint-config` to your devDependencies:
+
+```shell
+npm install --save-dev eslint @crycode/eslint-config
+```
+
+Add a file named `.eslintrc.js` to your project root directory:
+
+```js
+module.exports = {
+  extends: [
+    '@crycode/eslint-config'
+  ],
+  parserOptions: {
+    project: [
+      './tsconfig.json',
+    ],
+  },
+};
+```
+
+If you habe multiple tsconfig files, add them all to `project` like this:
+
+```js
+module.exports = {
+  extends: [
+    '@crycode/eslint-config'
+  ],
+  parserOptions: {
+    project: [
+      './tsconfig.backend.json',
+      './tsconfig.frontend.json',
+    ],
+  },
+};
+```
+
+## Changelog
+
+<!--
+    Placeholder for the next version (at the beginning of the line):
+    ### **WORK IN PROGRESS**
+-->
+
+### **WORK IN PROGRESS**
+
+* First release
+
+## License
+
+MIT License
+
+Copyright (c) 2021-2022 Peter Müller <peter@crycode.de>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ eslint-config.js
@@ -0,0 +1,397 @@
+/**
+ * ESLint configuration to be used with TypeScript according to my
+ * personal preferences.
+ */
+
+module.exports = {
+  parser: '@typescript-eslint/parser',
+
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    project: [
+      './tsconfig.json'
+    ],
+  },
+
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+  ],
+
+  plugins: [],
+
+  ignorePatterns: [
+    '/build/*',
+    '/dist/*',
+  ],
+
+  rules: {
+
+    'array-bracket-spacing': ['warn', 'always'],
+
+    'array-callback-return': 'warn',
+
+    'arrow-parens': ['warn', 'always'],
+
+    'arrow-spacing': 'warn',
+
+    'brace-style': 'off',
+    '@typescript-eslint/brace-style': [
+      'error',
+      '1tbs',
+      {
+        allowSingleLine: true,
+      },
+    ],
+
+    'comma-dangle': 'off',
+    '@typescript-eslint/comma-dangle': [
+      'warn',
+      {
+        arrays: 'always-multiline',
+        objects: 'always-multiline',
+        imports: 'always-multiline',
+        exports: 'always-multiline',
+        functions: 'always-multiline',
+        enums: 'always-multiline',
+        generics: 'always-multiline',
+        tuples: 'always-multiline',
+      },
+    ],
+
+    'comma-spacing': 'off',
+    '@typescript-eslint/comma-spacing': 'error',
+
+    '@typescript-eslint/consistent-type-assertions': [
+      'error',
+      {
+        assertionStyle: 'as',
+      },
+    ],
+
+    'default-param-last': 'off',
+    '@typescript-eslint/default-param-last': 'error',
+
+    'dot-location': ['warn', 'property'],
+
+    'eol-last': 'warn',
+
+    'eqeqeq': 'warn',
+
+    '@typescript-eslint/explicit-function-return-type': [
+      'error',
+      {
+        allowExpressions: true,
+      },
+    ],
+
+    '@typescript-eslint/explicit-member-accessibility': [
+      'error',
+      {
+        overrides: {
+          constructors: 'off',
+        },
+      },
+    ],
+
+    '@typescript-eslint/func-call-spacing': 'error',
+
+    'indent': 'off',
+    '@typescript-eslint/indent': [
+      'error',
+      2,
+      {
+        SwitchCase: 1,
+        MemberExpression: 'off',
+      },
+    ],
+
+    'jsx-quotes': ['error', 'prefer-single'],
+
+    'keyword-spacing': 'off',
+    '@typescript-eslint/keyword-spacing': 'error',
+
+    'linebreak-style': ['warn', 'unix'],
+
+    'lines-between-class-members': 'off',
+    '@typescript-eslint/lines-between-class-members': 'warn',
+
+    '@typescript-eslint/member-delimiter-style': [
+      'error',
+      {
+        multiline: {
+          delimiter: 'semi',
+          requireLast: true,
+        },
+        singleline: {
+          delimiter: 'comma',
+          requireLast: false,
+        },
+      },
+    ],
+
+    '@typescript-eslint/member-ordering': [
+      'warn',
+      {
+        default: [
+          // Index signature
+          //'signature',
+
+          // Fields
+          'public-abstract-field',
+          'protected-abstract-field',
+          'private-abstract-field',
+
+          'public-static-field',
+          'protected-static-field',
+          'private-static-field',
+
+          //'public-decorated-field',
+          //'protected-decorated-field',
+          //'private-decorated-field',
+
+          'public-instance-field',
+          'protected-instance-field',
+          'private-instance-field',
+
+          'public-field',
+          'protected-field',
+          'private-field',
+
+          'static-field',
+          'instance-field',
+          'abstract-field',
+
+          //'decorated-field',
+
+          'field',
+
+          // Constructors
+          'public-constructor',
+          'protected-constructor',
+          'private-constructor',
+
+          'constructor',
+
+          // Methods
+          'public-abstract-method',
+          'public-static-method',
+          //'public-decorated-method',
+          'public-instance-method',
+          'public-method',
+
+          'protected-abstract-method',
+          'protected-static-method',
+          //'protected-decorated-method',
+          'protected-instance-method',
+          'protected-method',
+
+          'private-abstract-method',
+          'private-static-method',
+          //'private-decorated-method',
+          'private-instance-method',
+          'private-method',
+
+          'abstract-method',
+          'static-method',
+          //'decorated-method',
+          'instance-method',
+
+          'method',
+        ],
+      },
+    ],
+
+    '@typescript-eslint/method-signature-style': 'warn',
+
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: ['enum', 'interface', 'class'],
+        format: ['PascalCase'],
+      },
+      {
+        selector: 'enumMember',
+        format: ['camelCase', 'UPPER_CASE'],
+      },
+    ],
+
+    'no-caller': 'error',
+
+    '@typescript-eslint/no-confusing-non-null-assertion': 'error',
+
+    'no-console': 'error',
+
+    'no-duplicate-imports': 'off',
+    '@typescript-eslint/no-duplicate-imports': [
+      'error',
+      {
+        includeExports: true,
+      },
+    ],
+
+    'no-eval': 'error',
+
+    'no-implied-eval': 'off',
+    '@typescript-eslint/no-implied-eval': 'error',
+
+    '@typescript-eslint/no-inferrable-types': 'off',
+
+    'no-invalid-this': 'off',
+    '@typescript-eslint/no-invalid-this': 'error',
+
+    'no-label-var': 'warn',
+
+    'no-loop-func': 'off',
+    '@typescript-eslint/no-loop-func': 'error',
+
+    'no-loss-of-precision': 'off',
+    '@typescript-eslint/no-loss-of-precision': 'error',
+
+    'no-mixed-operators': 'warn',
+
+    '@typescript-eslint/no-namespace': 'error',
+
+    '@typescript-eslint/no-parameter-properties': 'error',
+
+    'no-redeclare': 'off',
+    '@typescript-eslint/no-redeclare': 'error',
+
+    '@typescript-eslint/no-require-imports': 'error',
+
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': 'error',
+
+    '@typescript-eslint/no-throw-literal': 'error',
+
+    'no-trailing-spaces': 'warn',
+
+    'no-undef-init': 'warn',
+
+    '@typescript-eslint/no-unnecessary-type-assertion': 'warn',
+
+    '@typescript-eslint/no-unnecessary-type-constraint': 'warn',
+
+    '@typescript-eslint/no-unsafe-assignment': 'warn',
+
+    '@typescript-eslint/no-unsafe-call': 'warn',
+
+    'no-unused-expressions': 'off',
+    '@typescript-eslint/no-unused-expressions': 'warn',
+
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        ignoreRestSiblings: true,
+        argsIgnorePattern: '^_',
+      },
+    ],
+
+    '@typescript-eslint/no-use-before-define': 'error',
+
+    'no-var': 'error',
+
+    '@typescript-eslint/non-nullable-type-assertion-style': 'warn',
+
+    'object-curly-spacing': 'off',
+    '@typescript-eslint/object-curly-spacing': ['warn', 'always'],
+
+    '@typescript-eslint/prefer-includes': 'warn',
+
+    'prefer-const': 'error',
+
+    '@typescript-eslint/prefer-nullish-coalescing': 'warn',
+
+    '@typescript-eslint/prefer-optional-chain': 'warn',
+
+    '@typescript-eslint/prefer-string-starts-ends-with': 'warn',
+
+    '@typescript-eslint/prefer-ts-expect-error': 'warn',
+
+    '@typescript-eslint/promise-function-async': [
+      'error',
+      {
+        checkArrowFunctions: false,
+      },
+    ],
+
+    'quote-props': ['warn', 'as-needed'],
+
+    'quotes': 'off',
+    '@typescript-eslint/quotes': [
+      'error',
+      'single',
+      {
+        avoidEscape: true,
+        allowTemplateLiterals: true,
+      },
+    ],
+
+    'radix': 'warn',
+
+    '@typescript-eslint/restrict-plus-operands': [
+      'error',
+      {
+        checkCompoundAssignments: true,
+      },
+    ],
+
+    'no-return-await': 'off',
+    '@typescript-eslint/return-await': 'warn',
+
+    'semi': 'off',
+    '@typescript-eslint/semi': [
+      'warn',
+      'always',
+      {
+        omitLastInOneLineBlock: false,
+      },
+    ],
+
+    'semi-style': 'warn',
+
+    'sort-imports': [
+      'warn',
+      {
+        ignoreCase: true,
+        ignoreDeclarationSort: true,
+      },
+    ],
+
+    'space-before-blocks': 'warn',
+
+    'space-before-function-paren': 'off',
+    '@typescript-eslint/space-before-function-paren': 'warn',
+
+    'space-infix-ops': 'off',
+    '@typescript-eslint/space-infix-ops': [
+      'error',
+      {
+        int32Hint: false,
+      },
+    ],
+
+    'spaced-comment': [
+      'warn',
+      'always',
+      {
+        exceptions: ['-', '+', '*'],
+        line: {
+          markers: ['/'],
+        },
+        block: {
+          balanced: true,
+        },
+      },
+    ],
+
+    '@typescript-eslint/switch-exhaustiveness-check': 'warn',
+
+    '@typescript-eslint/type-annotation-spacing': 'warn',
+
+    '@typescript-eslint/unified-signatures': 'warn',
+
+    'yoda': 'warn',
+  },
+
+};
